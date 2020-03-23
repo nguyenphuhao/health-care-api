f1b81125caf7b75c7a8893b8926edb67e1172565
Hao Nguyen <Hao_Nguyen@epam.com>
2020-03-23 23:39:02 +0700

    fix: re-format code

--- controllers/periods/index.test.js
+++ controllers/periods/index.test.js
@@ -2,14 +2,14 @@ const moment = require('moment');
 const periods = require('./');
 test('getMenstrualCycle - stable', () => {
   const dateList = [
-    { 
+    {
       start: new moment('2019-11-10'),
-      end: new moment('2019-12-08') 
+      end: new moment('2019-12-08'),
     },
-    { 
+    {
       start: new moment('2019-12-08'),
-      end: new moment('2020-01-05') 
-    }
+      end: new moment('2020-01-05'),
+    },
   ];
   const result = periods.getMenstrualCycle(dateList);
   expect(result).toBe(28);
@@ -17,22 +17,22 @@ test('getMenstrualCycle - stable', () => {
 
 test('getMenstrualCycle - unstable', () => {
   const dateList = [
-    { 
+    {
       start: new moment('2019-11-10'),
-      end: new moment('2019-12-08') 
+      end: new moment('2019-12-08'),
     },
-    { 
+    {
       start: new moment('2019-12-08'),
-      end: new moment('2020-01-05') 
+      end: new moment('2020-01-05'),
     },
-    { 
+    {
       start: new moment('2020-01-05'),
-      end: new moment('2020-02-09') 
+      end: new moment('2020-02-09'),
     },
-    { 
+    {
       start: new moment('2020-02-09'),
-      end: new moment('2020-03-15') 
-    }
+      end: new moment('2020-03-15'),
+    },
   ];
   const result = periods.getMenstrualCycle(dateList);
   expect(result).toBe(31);
